a62a840e3991381fdd617f087d5605b63e1237db
MigelusMaximus <kurak.miso@gmail.com>
2023-10-29 12:08:17 +0100

    Added functionality and design to Destined For Greatness

--- Inspiration/Destined_For_Greatness/dfg.js
+++ Inspiration/Destined_For_Greatness/dfg.js
@@ -1,4 +1,3 @@
-
 var tag = document.createElement('script');
 window.onYouTubeIframeAPIReady =  ()=>{
     
@@ -9,9 +8,63 @@ window.onYouTubeIframeAPIReady =  ()=>{
         height: '720',
         width: '1280',
         videoId: '1FJRvKQmOwg',
+        events: {
+            'onStateChange': onPlayerStateChange
+          }
     
       });
     };
+
+    function setTransition(element,duration){
+        element.style.transition =`background-color ${duration}s,opacity ${duration}s `;
+    }
+    function setTransitionOpacity(element,duration){
+        element.style.transition =`opacity ${duration}s `;
+    }
+    function changeBackgroundColor(element,newColor){
+        element.style.backgroundColor=newColor;
+    }
+    function changeOpacity(element, opacityValue){
+        element.style.opacity = opacityValue;
+    }
+    function setTransitionColor(element,duration){
+        element.style.transition =`color ${duration}s `;
+    }
+    function changeColor(element, newColor){
+        element.style.color = newColor;
+    }
       tag.src = "https://www.youtube.com/iframe_api";
       var firstScriptTag = document.getElementsByTagName('script')[0];
       firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+      function onPlayerStateChange(event) {
+        const body = document.body;
+        const navigation_background = document.getElementById("navigation");
+        const DFG_name = document.getElementById("DFG_name");
+        if(event.data == 1) {
+
+            setTransition(body,3);
+            setTransitionOpacity(navigation_background,3);
+            setTransitionColor(DFG_name,6);
+            changeColor(DFG_name,"#ffe680")
+            
+
+            
+            changeBackgroundColor(body, "#000000");
+            changeOpacity(navigation_background,"0");
+            
+            
+
+        }
+        
+        else if(event.data == 2) {
+            
+
+            navigation_background.style.opacity ="1";
+            changeOpacity(navigation_background,"1");
+            changeBackgroundColor(body, "#e76f51");
+            changeColor(DFG_name,"black")
+
+            
+        }
+  }
